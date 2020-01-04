52ad73030d5721482e3a03fc8b3772367787a569
Matt Chapman <chapabu101@gmail.com>
2020-01-04 04:38:15 +0000

    Remove unused code

--- template/src/components/app.tsx
+++ template/src/components/app.tsx
@@ -1,25 +1,19 @@
 import { h } from 'preact';
-import { Route, Router, RouterOnChangeArgs } from 'preact-router';
+import { Route, Router } from 'preact-router';
 
 import Home from '../routes/home';
 import Profile from '../routes/profile';
 import Header from './header';
 
-if ((module as any).hot) {
-  // tslint:disable-next-line:no-var-requires
-  require('preact/debug');
+if ((module as NodeModule).hot) {
+  require('preact/debug'); // eslint-disable-line global-require
 }
 
 const App: preact.FunctionalComponent = () => {
-  let currentUrl: string;
-  const handleRoute = (e: RouterOnChangeArgs) => {
-    currentUrl = e.url;
-  };
-
   return (
     <div id="app">
       <Header />
-      <Router onChange={handleRoute}>
+      <Router>
         <Route path="/" component={Home} />
         <Route path="/profile/" component={Profile} user="me" />
         <Route path="/profile/:user" component={Profile} />
