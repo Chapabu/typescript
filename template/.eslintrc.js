ed62b9f4019d277d3c8120102be1e90999daa5e2
Matt Chapman <chapabu101@gmail.com>
2020-01-04 03:37:41 +0000

    Fix ESLint env and react/preact incompatibilites.

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,23 +1,30 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    extends: [
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'airbnb',
-        'airbnb/hooks',
-        'plugin:import/typescript'
-    ],
-    rules: {
-        // Disable whilst we wait for the fix for
-        // https://github.com/benmosher/eslint-plugin-import/issues/1558 to be released.
-        'import/extensions': 0,
-        'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }]
-    },
-    settings: {
-        react: {
-            pragma: 'h'
-        }
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:import/typescript'
+  ],
+  env: {
+    browser: true,
+    jest: true
+  },
+  rules: {
+    // Disable whilst we wait for the fix for
+    // https://github.com/benmosher/eslint-plugin-import/issues/1558 to be released.
+    'import/extensions': 0,
+    'react/prop-types': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
+    'react/no-unknown-property': [1, { ignore: ['class'] }],
+    'react/jsx-one-expression-per-line': [1, { allow: 'literal' }]
+  },
+  settings: {
+    react: {
+      pragma: 'h'
     }
+  }
 };
