af267c95befbd6da0ce735ffe788faa6865f735d
Matt Chapman <chapabu101@gmail.com>
2020-01-04 03:20:19 +0000

    Fix import and JSX filename extension warnings

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -3,8 +3,21 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     extends: [
-        'airbnb/hooks',
         'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended'
-    ]
+        'plugin:@typescript-eslint/recommended',
+        'airbnb',
+        'airbnb/hooks',
+        'plugin:import/typescript'
+    ],
+    rules: {
+        // Disable whilst we wait for the fix for
+        // https://github.com/benmosher/eslint-plugin-import/issues/1558 to be released.
+        'import/extensions': 0,
+        'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }]
+    },
+    settings: {
+        react: {
+            pragma: 'h'
+        }
+    }
 };
