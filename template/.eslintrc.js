1e69c0e09963140f82bae97b99028ce433392bbc
Matt Chapman <chapabu101@gmail.com>
2020-01-04 03:53:26 +0000

    Fix preact config file

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -18,11 +18,26 @@ module.exports = {
     // https://github.com/benmosher/eslint-plugin-import/issues/1558 to be released.
     'import/extensions': 0,
     'arrow-body-style': 0,
+    'comma-dangle': [
+      'error',
+      {
+        functions: 'never'
+      }
+    ],
     'react/prop-types': 0,
     'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
     'react/no-unknown-property': [1, { ignore: ['class'] }],
     'react/jsx-one-expression-per-line': [1, { allow: 'literal' }]
   },
+  overrides: [
+    {
+      // enable the rule specifically for TypeScript files
+      files: ['*.js', '*.jsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 0
+      }
+    }
+  ],
   settings: {
     react: {
       pragma: 'h'
